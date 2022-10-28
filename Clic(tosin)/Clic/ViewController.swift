672e547a33fc62f51b3332f96d318182c80e0815
gbktosin <104964198+gbktosin@users.noreply.github.com>
2022-10-28 16:26:44 -0400

    Added Pods

---
+++ Clic(tosin)/Clic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clic
+//
+//  Created by Ritik Gupta on 10/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
