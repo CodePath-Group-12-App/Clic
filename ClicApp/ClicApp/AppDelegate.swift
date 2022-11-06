aac8413a1cdf9d73b8a36cb345d7bcd12218e648
Ritik <ritik3411@gmail.com>
2022-11-06 00:21:00 -0400

    Google Sign In
    Added a google sign in button, it doesn't work properly however. Not sure how to fix it. We can scrap it later.
    
    Issues:
    1.sign in fails, enters the user in which is not good.
    
    2. When pop up appears, it shows the tab bar menu instead of the login in screen.
    
    If those two issues get fix then the google button should work as an extra login

--- ClicApp/ClicApp/AppDelegate.swift
+++ ClicApp/ClicApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import CoreData
+import GoogleSignIn
+import GoogleSignInSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let signInConfig = GIDConfiguration(clientID: "597761135101-20b3ssrtsm6qa1qb9e558p2h71eq930g.apps.googleusercontent.com")
+        
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+          if error != nil || user == nil {
+            // Show the app's signed-out state.
+          } else {
+            // Show the app's signed-in state.
+          }
+        }
         return true
     }
 
@@ -30,6 +42,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      //  return (GIDSignInButton.sharedInstance()?.handle(url))!
+        
+        var handled: Bool
+
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+          return true
+        }
+
+        // Handle other custom URL types.
+
+        // If not handled by this app, return false.
+        return false
     }
 
     // MARK: - Core Data stack
