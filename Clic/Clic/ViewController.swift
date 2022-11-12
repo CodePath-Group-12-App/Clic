09b1e8f4b8f456ae356fd68d499c5b9265250c63
gbktosin <104964198+gbktosin@users.noreply.github.com>
2022-11-12 07:43:46 -0500

    11/12/22

---
+++ Clic/Clic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clic
+//
+//  Created by Jesse Oseafiana on 11/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
